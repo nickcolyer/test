70edd1f551c7d79b34f2a1d514709bf36d32c7ba
Nick Colyer <nickcolyer@gmail.com>
2021-01-06 11:06:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS2020-Azure-Linux-VM
+ Demo of Linux VM Deployment in Azure
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+
+
